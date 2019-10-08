0ce580bf1b212ca1f5ee80dda59be9884fe49671
Eytan Avisror <eytan_avisror@intuit.com>
2019-10-08 10:53:31 -0700

    release

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,15 +1,15 @@
 package version
 
 import (
-        "runtime"
-        "fmt"
+	"fmt"
+	"runtime"
 )
 
 // The git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
 
 // The main version number that is being run at the moment.
-const Version = "0.1.0"
+const Version = "0.2.0"
 
 var BuildDate = ""
 
