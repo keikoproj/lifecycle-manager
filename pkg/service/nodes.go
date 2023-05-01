b418e23725ef67d953ff1e089528072854ef59a8
sbadiger <shreyas_badiger@intuit.com>
2023-05-01 09:25:16 -0700

    make drain-retries configurable
    Signed-off-by: sbadiger <shreyas_badiger@intuit.com>

--- pkg/service/nodes.go
+++ pkg/service/nodes.go
@@ -49,7 +49,7 @@ func isNodeStatusInCondition(node v1.Node, condition v1.ConditionStatus) bool {
 	return false
 }
 
-func drainNode(kubectlPath, nodeName string, timeout, retryInterval int64) error {
+func drainNode(kubectlPath, nodeName string, timeout, retryInterval, retryAttempts int64) error {
 	drainArgs := []string{"drain", nodeName, "--ignore-daemonsets=true", "--delete-local-data=true", "--force", "--grace-period=-1"}
 	drainCommand := kubectlPath
 
@@ -58,7 +58,7 @@ func drainNode(kubectlPath, nodeName string, timeout, retryInterval int64) error
 		return nil
 	}
 
-	err := runCommandWithContext(drainCommand, drainArgs, timeout, retryInterval)
+	err := runCommandWithContext(drainCommand, drainArgs, timeout, retryInterval, retryAttempts)
 	if err != nil {
 		if err.Error() == "command execution timed out" {
 			log.Warnf("failed to drain node %v, drain command timed-out", nodeName)
@@ -70,7 +70,7 @@ func drainNode(kubectlPath, nodeName string, timeout, retryInterval int64) error
 	return nil
 }
 
-func runCommandWithContext(call string, args []string, timeoutSeconds, retryInterval int64) error {
+func runCommandWithContext(call string, args []string, timeoutSeconds, retryInterval, retryAttempts int64) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeoutSeconds)*time.Second)
 	defer cancel()
 	err := retry.Do(
@@ -89,7 +89,7 @@ func runCommandWithContext(call string, args []string, timeoutSeconds, retryInte
 			}
 			return false
 		}),
-		retry.Attempts(3),
+		retry.Attempts(retryAttempts),
 		retry.Delay(time.Duration(retryInterval)*time.Second),
 	)
 	if err != nil {
